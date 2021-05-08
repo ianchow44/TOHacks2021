da9e39d14cbb3e58a0935c88814f259111df0363
Tony Huang <t.tonyhuang@gmail.com>
2021-05-08 15:54:14 -0400

    finish saving images

--- tohacks2021/src/apps/GoogleVision/components/ImageSelector.jsx
+++ tohacks2021/src/apps/GoogleVision/components/ImageSelector.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-export default function ImageSelector() {
+export default function ImageSelector({onChange}) {
     return (
         <div>
             <Button
@@ -12,6 +12,7 @@ export default function ImageSelector() {
                 <input
                     type="file"
                     hidden
+                    onChange = {(e)=> onChange(e)}
                 />
             </Button>
         </div>
