88c7dd392f59605b7145b89c65226d567ea40fca
Tony Huang <t.tonyhuang@gmail.com>
2021-05-08 12:13:30 -0400

    add routing

--- tohacks2021/src/components/App.js
+++ tohacks2021/src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React from "react";
 import Signup from "./signup";
-
+import GoogleVision from "./GoogleVision";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return <Signup/>
+  <Router>
+    <div className="container">
+      <Route path="/" exact component={Signup} />
+      <Route path="/faceauth" component={GoogleVision} />
+    </div>
+  </Router>;
 }
 
 export default App;
---
+++ tohacks2021/src/components/GoogleVision.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function GoogleVision() {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
